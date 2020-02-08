d473ae12e899b800d6df00b1b3e3b3895f05554d
shchennikov <shchennikov@yandex.ru>
2020-02-08 18:58:10 +0300

    webpack_build__clear

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import * as $ from 'jquery'
+import Post from '@models/Post'
+import json from './assets/json'
+import WebpackLogo from './assets/webpack-logo'
+import xml from './assets/data.xml'
+import csv from './assets/data.csv'
+import './styles/styles'
+
+const post = new Post('Webpack Post Title', WebpackLogo)
+
+$('pre').addClass('code').html(post.toString())
+
+console.log('JSON:' , json)
+console.log('XML:', xml)
+console.log('CSV:', csv)
