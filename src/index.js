75b85a1ae942cb799b4a05440269e70ecf768f62
shchennikov <shchennikov@yandex.ru>
2020-02-09 12:33:20 +0300

    v.1.0.0

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import * as $ from 'jquery'
 import Post from '@models/Post'
+import './babel'
 import json from './assets/json'
 import WebpackLogo from './assets/webpack-logo'
 import xml from './assets/data.xml'
 import csv from './assets/data.csv'
 import './styles/styles'
+import './styles/less.less'
+import  './styles/scss.scss'
 
 const post = new Post('Webpack Post Title', WebpackLogo)
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,55 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const cssLoaders = extra => {
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+        },'css-loader'
+    ]
+
+    if(extra) {
+        loaders.push(extra)
+    }
+
+    return loaders
+}
+
+
+const optimization = () => {
+    const config = {
+        splitChunks: {chunks: "all"}
+    }
+
+    if (isProd){
+        config.minimizer = [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin(),
+        ]
+    }
+
+
+    return config
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: "development",
 
     entry: {
-      main: './index.js',
+      main: ['@babel/polyfill','./index.js'],
       analytics: './analytics.js'
     },
     output: {
@@ -23,14 +64,16 @@ module.exports = {
         }
     },
     devServer: {
-        port: 4200
+        port: 4200,
+        hot: isDev,
     },
-    optimization: {
-        splitChunks: {chunks: "all"}
-    },
+    optimization: optimization(),
     plugins: [
         new HTMLWebpackPlugin({
             template: "./index.html",
+            minify: {
+                collapseWhitespace: isProd
+            }
         }),
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin([
@@ -38,15 +81,27 @@ module.exports = {
                 from: path.resolve(__dirname, 'src/favicon.ico'),
                 to: path.resolve(__dirname, 'dist')
             }
-        ])
+        ]),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+        })
     ],
 
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader','css-loader']
+                use: cssLoaders()
             },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader',)
+            },
+            {
+                test: /\.less$/,
+                use: cssLoaders('less-loader')
+            },
+
             {
                 test: /\.(png|jpg|svg|gif)$/,
                 use: ['file-loader']
@@ -63,6 +118,18 @@ module.exports = {
                 test: /\.csv$/,
                 use: ['csv-loader']
             },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env"
+                        ]
+                    }
+                }
+            }
         ]
     }
 }
